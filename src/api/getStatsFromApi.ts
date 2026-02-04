f0e5f5c1346b1fea57afae8d670fc87bd93659ff
Samuel Hellstrøm <hellstromsamuel@gmail.com>
2026-02-04 21:52:00 +0100

    Enhance .env.example with admin secret; refactor getStatsFromApi to include admin secret and streamline token retrieval

--- src/api/getStatsFromApi.ts
+++ src/api/getStatsFromApi.ts
@@ -1,39 +1,56 @@
 import { ICollectionStat } from "@/shared/interfaces/ICollectionStat";
 
+async function getToken(
+  baseUrl: string,
+  clientId: string,
+  clientSecret: string
+): Promise<string> {
+  const response = await fetch(`${baseUrl}/api_clients/token`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ clientId, clientSecret }),
+  });
+
+  if (!response.ok) {
+    throw new Error("Failed to get token");
+  }
+
+  const tokenData = await response.json();
+  return tokenData.token;
+}
+
 export async function getStatsFromApi(): Promise<ICollectionStat[]> {
   const baseUrl = process.env.EASYPLAY_API_BASE_URL;
   const clientId = process.env.EASYPLAY_API_CLIENT_ID;
   const clientSecret = process.env.EASYPLAY_API_CLIENT_SECRET;
+  const adminSecret = process.env.EASYPLAY_API_ADMIN_SECRET;
+
+  if (!baseUrl) {
+    throw new Error("Missing API base URL");
+  }
+
+  if (!clientId) {
+    throw new Error("Missing client ID");
+  }
+
+  if (!clientSecret) {
+    throw new Error("Missing client secret");
+  }
+
+  if (!adminSecret) {
+    throw new Error("Missing admin secret");
+  }
 
   try {
-    if (!baseUrl) {
-      throw new Error("Missing API base URL");
-    }
-
-    if (!clientId || !clientSecret) {
-      throw new Error("Missing client ID or client secret");
-    }
-
-    const token = await fetch(`${baseUrl}/api_clients/token`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ clientId, clientSecret }),
-    });
-
-    if (!token.ok) {
-      throw new Error("Failed to get token");
-    }
-
-    const tokenData = await token.json();
-    const authToken = tokenData.token;
-
+    const token = await getToken(baseUrl, clientId, clientSecret);
     const response = await fetch(baseUrl + "/stats", {
       method: "GET",
       headers: {
-        Authorization: `Bearer ${authToken}`,
+        Authorization: `Bearer ${token}`,
         "Content-Type": "application/json",
+        "x-admin-secret": adminSecret,
       },
       cache: "no-store",
     });
